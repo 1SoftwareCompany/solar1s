4f0767073c457242324e76dc988676e9ab514e4c
mynkow <mynkow@gmail.com>
2025-05-08 12:46:29 +0300

    trying other way to configure entity

--- custom_components/solaris/__init__.py
+++ custom_components/solaris/__init__.py
@@ -1,9 +1,13 @@
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.discovery import async_load_platform
 from .const import DOMAIN
 
 async def async_setup(hass: HomeAssistant, config: dict):
     hass.data[DOMAIN] = {}
-    return True
 
-async def async_setup_entry(hass, config_entry):
+    # Load the sensor platform
+    hass.async_create_task(
+        async_load_platform(hass, "sensor", DOMAIN, {}, config)
+    )
+
     return True
--- custom_components/solaris/sensor.py
+++ custom_components/solaris/sensor.py
@@ -1,30 +1,8 @@
-from homeassistant.helpers.entity import Entity
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.discovery import async_load_platform
-from .const import DOMAIN
+from homeassistant.components.sensor import SensorEntity
 
-async def async_setup_platform(hass: HomeAssistant, config, async_add_entities, discovery_info=None):
-    async_add_entities([EmployeeSensor()], True)
-
-class EmployeeSensor(Entity):
+class EmployeeSensor(SensorEntity):
     def __init__(self):
-        self._name = "mynkow"
-        self._state = "active"
-
-    @property
-    def name(self):
-        return f"Employee {self._name}"
-
-    @property
-    def state(self):
-        return self._state
-
-    @property
-    def unique_id(self):
-        return "employee_mynkow"
-
-    @property
-    def extra_state_attributes(self):
-        return {
-            "name": self._name
-        }
+        self._attr_name = "Employee Mynkow"
+        self._attr_unique_id = "employee_mynkow"
+        self._attr_native_value = "active"
+        self._attr_extra_state_attributes = {"name": "mynkow"}
